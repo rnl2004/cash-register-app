b612afd547e7a41a4f155f714b879fc909509239
Rico Lugod <rico.lugod@fissara.com>
2021-03-17 11:52:23 +0800

    refator object holder

--- src/test/java/com/rocketmiles/cashregister/CashRegisterAppApplicationTest.java
+++ src/test/java/com/rocketmiles/cashregister/CashRegisterAppApplicationTest.java
@@ -19,7 +19,7 @@ class CashRegisterAppApplicationTest {
         Integer[] putBills;
         Integer[] putBills2;
         Integer[] takeBills;
-        Integer expectedCurrentState;
+        Integer actualCurrentState;
         Map<Integer, Integer> currentStateResult;
 
         currentState = new HashMap<>();
@@ -33,46 +33,46 @@ class CashRegisterAppApplicationTest {
         putBills = new Integer[]{1, 2, 3, 4, 5};
         billsService.put(currentState, putBills);
         currentStateResult = billsService.show(currentState);
-        expectedCurrentState = currentStateResult.get(DollarBillsEnum.TWENTY_DOLLAR_BILL.getValue());
-        Assertions.assertEquals(1, expectedCurrentState);
-        expectedCurrentState = currentStateResult.get(DollarBillsEnum.TEN_DOLLAR_BILL.getValue());
-        Assertions.assertEquals(2, expectedCurrentState);
-        expectedCurrentState = currentStateResult.get(DollarBillsEnum.FIVE_DOLLAR_BILL.getValue());
-        Assertions.assertEquals(3, expectedCurrentState);
-        expectedCurrentState = currentStateResult.get(DollarBillsEnum.TWO_DOLLAR_BILL.getValue());
-        Assertions.assertEquals(4, expectedCurrentState);
-        expectedCurrentState = currentStateResult.get(DollarBillsEnum.ONE_DOLLAR_BILL.getValue());
-        Assertions.assertEquals(5, expectedCurrentState);
+        actualCurrentState = currentStateResult.get(DollarBillsEnum.TWENTY_DOLLAR_BILL.getValue());
+        Assertions.assertEquals(1, actualCurrentState);
+        actualCurrentState = currentStateResult.get(DollarBillsEnum.TEN_DOLLAR_BILL.getValue());
+        Assertions.assertEquals(2, actualCurrentState);
+        actualCurrentState = currentStateResult.get(DollarBillsEnum.FIVE_DOLLAR_BILL.getValue());
+        Assertions.assertEquals(3, actualCurrentState);
+        actualCurrentState = currentStateResult.get(DollarBillsEnum.TWO_DOLLAR_BILL.getValue());
+        Assertions.assertEquals(4, actualCurrentState);
+        actualCurrentState = currentStateResult.get(DollarBillsEnum.ONE_DOLLAR_BILL.getValue());
+        Assertions.assertEquals(5, actualCurrentState);
 
         /** TC Scenario 2 */
         putBills2 = new Integer[]{1, 2, 3, 0, 5};
         billsService.put(currentState, putBills2);
         currentStateResult = billsService.show(currentState);
-        expectedCurrentState = currentStateResult.get(DollarBillsEnum.TWENTY_DOLLAR_BILL.getValue());
-        Assertions.assertEquals(2, expectedCurrentState);
-        expectedCurrentState = currentStateResult.get(DollarBillsEnum.TEN_DOLLAR_BILL.getValue());
-        Assertions.assertEquals(4, expectedCurrentState);
-        expectedCurrentState = currentStateResult.get(DollarBillsEnum.FIVE_DOLLAR_BILL.getValue());
-        Assertions.assertEquals(6, expectedCurrentState);
-        expectedCurrentState = currentStateResult.get(DollarBillsEnum.TWO_DOLLAR_BILL.getValue());
-        Assertions.assertEquals(4, expectedCurrentState);
-        expectedCurrentState = currentStateResult.get(DollarBillsEnum.ONE_DOLLAR_BILL.getValue());
-        Assertions.assertEquals(10, expectedCurrentState);
+        actualCurrentState = currentStateResult.get(DollarBillsEnum.TWENTY_DOLLAR_BILL.getValue());
+        Assertions.assertEquals(2, actualCurrentState);
+        actualCurrentState = currentStateResult.get(DollarBillsEnum.TEN_DOLLAR_BILL.getValue());
+        Assertions.assertEquals(4, actualCurrentState);
+        actualCurrentState = currentStateResult.get(DollarBillsEnum.FIVE_DOLLAR_BILL.getValue());
+        Assertions.assertEquals(6, actualCurrentState);
+        actualCurrentState = currentStateResult.get(DollarBillsEnum.TWO_DOLLAR_BILL.getValue());
+        Assertions.assertEquals(4, actualCurrentState);
+        actualCurrentState = currentStateResult.get(DollarBillsEnum.ONE_DOLLAR_BILL.getValue());
+        Assertions.assertEquals(10, actualCurrentState);
 
         /** TC Scenario 3 */
         takeBills = new Integer[]{1, 4, 3, 0, 10};
         billsService.take(currentState, takeBills);
         currentStateResult = billsService.show(currentState);
-        expectedCurrentState = currentStateResult.get(DollarBillsEnum.TWENTY_DOLLAR_BILL.getValue());
-        Assertions.assertEquals(1, expectedCurrentState);
-        expectedCurrentState = currentStateResult.get(DollarBillsEnum.TEN_DOLLAR_BILL.getValue());
-        Assertions.assertEquals(0, expectedCurrentState);
-        expectedCurrentState = currentStateResult.get(DollarBillsEnum.FIVE_DOLLAR_BILL.getValue());
-        Assertions.assertEquals(3, expectedCurrentState);
-        expectedCurrentState = currentStateResult.get(DollarBillsEnum.TWO_DOLLAR_BILL.getValue());
-        Assertions.assertEquals(4, expectedCurrentState);
-        expectedCurrentState = currentStateResult.get(DollarBillsEnum.ONE_DOLLAR_BILL.getValue());
-        Assertions.assertEquals(0, expectedCurrentState);
+        actualCurrentState = currentStateResult.get(DollarBillsEnum.TWENTY_DOLLAR_BILL.getValue());
+        Assertions.assertEquals(1, actualCurrentState);
+        actualCurrentState = currentStateResult.get(DollarBillsEnum.TEN_DOLLAR_BILL.getValue());
+        Assertions.assertEquals(0, actualCurrentState);
+        actualCurrentState = currentStateResult.get(DollarBillsEnum.FIVE_DOLLAR_BILL.getValue());
+        Assertions.assertEquals(3, actualCurrentState);
+        actualCurrentState = currentStateResult.get(DollarBillsEnum.TWO_DOLLAR_BILL.getValue());
+        Assertions.assertEquals(4, actualCurrentState);
+        actualCurrentState = currentStateResult.get(DollarBillsEnum.ONE_DOLLAR_BILL.getValue());
+        Assertions.assertEquals(0, actualCurrentState);
 
         /** TC Scenario 4 */
         billsService.change(currentState, 11);
